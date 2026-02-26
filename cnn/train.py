8c764269c4750b349d8524048d92da27db3823f9
Sarwat Ali <68585738+sarvatmir@users.noreply.github.com>
2026-02-26 11:56:17 +0530

    Update train.py

--- cnn/train.py
+++ cnn/train.py
@@ -20,13 +20,13 @@ from model import NetworkCIFAR as Network
 parser = argparse.ArgumentParser("cifar")
 parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+parser.add_argument('--learning_rate', type=float, default=0.01, help='init learning rate') #default=0.025
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
-parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
+parser.add_argument('--init_channels', type=int, default=16, help='num of init channels') # 36
 parser.add_argument('--layers', type=int, default=6, help='total number of layers')  # default is 20
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
